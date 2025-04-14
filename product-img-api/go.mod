b85a039c5bf55efdda0b93d6c0cb82fed87ed3f1
aRKO872 <arkomitra872@gmail.com>
2025-04-15 01:25:56 +0530

    added starter files and handlers to this, has a post and get call to upload and retrieve pics, both from local storage

---
+++ product-img-api/go.mod
@@ -0,0 +1,19 @@
+module github.com/product-img-api-microservice
+
+go 1.24.1
+
+require (
+	github.com/gorilla/handlers v1.5.2
+	github.com/gorilla/mux v1.8.1
+	github.com/hashicorp/go-hclog v1.6.3
+	github.com/nicholasjackson/building-microservices-youtube/product-images v0.0.0-20211011132451-0cd586295712
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+)
---
+++ product-img-api/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	hclog "github.com/hashicorp/go-hclog"
+	"github.com/product-img-api-microservice/files"
+	"github.com/product-img-api-microservice/handlers"
+
+	gohandler "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	var basePath = "./imagestore"
+
+	sm := mux.NewRouter()
+
+	l := hclog.New(
+		&hclog.LoggerOptions{
+			Name:  "product-images",
+		},
+	)
+
+	// create a logger for the server from the default logger
+	sl := l.StandardLogger(&hclog.StandardLoggerOptions{InferLevels: true})
+
+	// create the storage class, use local storage
+	// max filesize 5MB
+	stor, err := files.NewLocal(basePath, 1024*1000*5)
+	if err != nil {
+		l.Error("Unable to create storage", "error", err)
+		os.Exit(1)
+	}
+
+	// create the handlers
+	fh := handlers.NewFiles(stor, l)
+
+	ph := sm.Methods(http.MethodPost).Subrouter()
+	ph.HandleFunc("/images/{id:[0-9]+}/{filename}", fh.ServeHTTP)
+
+	gh := sm.Methods(http.MethodGet).Subrouter()
+	gh.Handle("/images/{id:[0-9]+}/{filename}", http.StripPrefix("/images", http.FileServer(http.Dir(basePath))))
+
+	// CORS
+	// We can add multiple referrers which can call this. Just adding one for the time being
+	ch := gohandler.CORS(gohandler.AllowedOrigins([]string{"http://localhost:8000"}))
+
+	// To allow everyone to access : 
+	// ch := gohandler.CORS(gohandler.AllowedOrigins([]string{"*"}))
+
+	// create a new server
+	s := http.Server{
+		Addr:         ":8080",      // configure the bind address
+		Handler:      ch(sm),                // set the default handler
+		ErrorLog:     sl,                // the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	go func () {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Error(err.Error())
+			os.Exit(1)
+		}
+	}()
+
+	// Process for graceful shutdown
+
+	sigChan := make(chan os.Signal, 2)
+
+	signal.Notify(sigChan, os.Interrupt)
+
+	sig := <-sigChan
+
+	l.Info("Graceful Shutdown, due to :", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+}
